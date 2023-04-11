11b97304f0a2235db321f350366e6c9f29b1a8f3
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-11 11:44:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const arr = []
+function writeCards(arr,event){
+    for (let i=0; i< arr.length;i++){
+        arr[i]=`Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+        
+    }
+    return arr
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num--)
+    }
+}
